eddbf23f5b7a1282ce38e07394e783c606c8b8ae
Douglas-CRVG <douglas.07.nascimento@gmail.com>
2022-04-24 15:21:34 -0300

    start

---
+++ src/Middlewares/errorHandlerMiddleware.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+
+interface ErrorHandler{
+    bad_request: number;
+    unauthorized: number;
+    not_found: number;
+    conflict: number;
+    unprocessable_entity: 422;
+}
+
+export default function errorHandlerMiddleware(error: any, request: Request, res: Response, next: NextFunction){
+    const errorTypes: ErrorHandler = {
+        bad_request: 400,
+        unauthorized: 401,
+        not_found: 404,
+        conflict: 409,
+        unprocessable_entity: 422
+    }
+
+    console.log(error);
+
+    if (!errorTypes[error.type]) {
+        return res.sendStatus(500);
+    }
+
+    return res.sendStatus(errorTypes[error.type]);
+}
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import "express-async-errors";
+import errorHandlerMiddleware from "./Middlewares/errorHandlerMiddleware.js";
+import router from "./Routes/index.js"
+dotenv.config();
+
+const server = express();
+server.use(cors());
+server.use(json());
+server.use(router);
+server.use(errorHandlerMiddleware)
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+    console.log(`Running on port ${PORT}`);
+});
